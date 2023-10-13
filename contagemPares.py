b5bc5c2510d2f771b12cfe0d7557c857e715451e
Francisco Ferreira de Araujo <fferreira.araujo@hotmail.com>
2023-10-13 13:57:28 -0300

    Tudo certo

--- contagemPares.py
+++ contagemPares.py
@@ -5,7 +5,7 @@ print (' O JOGO VAI COMEÇAR!!!!')
 print ('CONTAGEM NÚMEROS PARES, VAMOS LÁ!!!')
 
 for c in range (0,50+1,2):
-    print(c)
-    sleep(1)
+    print(c,end =' ')
+       
 print('\n')
 print ('VAI!')
--- somaImpares.py
+++ somaImpares.py
@@ -2,11 +2,13 @@
 print ('SOMA ENTRE OS NÚMEROS ÍMPARES e multiplos de 3, VAMOS LÁ!!!')
 
 s=0
+cont=0
 for c in range (0,500+1):
     if c %3==0 and c%2==1:
-        print(c, end=",")
+        cont +=1
+        print(c, end= ",")
         s+=c
 
 print('\n')
-print ('A soma entre ambos deu {}'.format(s))
+print ('A soma entre ambos deu {} e a quantidade de numero impares e multiplos de três são {}.'.format(s,cont))
 print ('VAI!')
