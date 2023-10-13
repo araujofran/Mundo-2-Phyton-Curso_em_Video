cbfeea374c309315c574f348836392b9117dcc74
Francisco Ferreira de Araujo <fferreira.araujo@hotmail.com>
2023-10-13 06:21:25 -0300

    Tudo certo!

--- pedraPapelTesoura.py
+++ pedraPapelTesoura.py
@@ -15,36 +15,43 @@ print ('-=-'*10)
 print ('----- Vamos jogar JOKENPÔ!!! -------')
 
 print (' Veja as opões: \n [0] PEDRA \n [1] TESOURA \n [2] PAPEL')
+print ('-=-'*15)
+jogue = int (input('ESCOLHA\n \n JO\nKEN\nPÔ\n\n'))
 
-jogue = int (input('Escolha \n JO\nKEN\nPÔ:'))
 
+print ('-=-'*15)
 
 print (' O computador jogou: {}.'.format(computador))
 print (' Você jogou: {}.'.format(jogue))
-       
+
+ 
 '''
 [0] PEDRA 
 [1] TESOURA 
 [2] PAPEL
 '''
+print ('-=-'*15)
+
 if computador == jogue:
     print('EMPATE')
 elif computador==0 and jogue==1: # (pedra x tesoura)
-    print( 'Você perdeu, pedra ganha para tesoura! ')
+    print( 'COMPUTADOR VENCE! ')
 elif computador==0 and jogue==2: # (pedra x papel)
-    print( 'Você ganhou, pedra perde para papel! ')
+    print( 'VOCE GANHOU! ')
 elif computador==1 and jogue==2: # (tesoura x papel)
-    print( 'Você perdeu, tesoura corta papel! ')
+    print( 'COMPUTADOR VENCE! ')
 elif computador==1 and jogue==0: # (tesoura x pedra)
-    print( 'Você ganhou, tesoura quebra na pedra! ')
+    print( 'VOCE GANHOU! ')
 
 elif computador==2 and jogue==1: # (papel x tesoura)
-    print( 'Você ganhou, papel é cortado pela tesoura! ')
+    print( 'VOCE GANHOU ')
 elif computador==2 and jogue==0: # (papel x pedra)
-    print( 'Você perdeu, papel abraça a pedra! ')
+    print( 'COMPUTADOR VENCE! ')
 
 print ('-=-'*15)
 
 print ('Quer Jogar de Novo???')
+
+print ('-=-'*15)
 
 
