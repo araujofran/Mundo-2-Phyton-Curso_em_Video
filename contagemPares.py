157a8d341794186be27c637a038b8c3f315da73c
Francisco Ferreira de Araujo <fferreira.araujo@hotmail.com>
2023-10-13 10:09:08 -0300

    Aprendendo Controladores no Python

---
+++ contagemPares.py
@@ -0,0 +1,11 @@
+from time import sleep
+
+print (' O JOGO VAI COMEÇAR!!!!')
+
+print ('CONTAGEM NÚMEROS PARES, VAMOS LÁ!!!')
+
+for c in range (0,50+1,2):
+    sleep(1)
+    print(c)
+print('\n')
+print ('VAI!')
--- contagemRegressiva.py
+++ contagemRegressiva.py
@@ -1,1 +1,13 @@
+from time import sleep
 
+print (' O JOGO VAI COMEÇAR!!!!')
+
+print ('CONTAGEM REGRESSIVA, VAMOS LÁ!!!')
+
+for c in range (10,-1,-1):
+    sleep(1)
+    
+    print(c)
+    
+print('\n')
+print ('VAI!')
